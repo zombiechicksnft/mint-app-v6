d6dddbe263eabe0d9e09c71d69374cf9be1c6527
zombiechicksnft <97534834+zombiechicksnft@users.noreply.github.com>
2022-01-15 09:02:42 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -263,8 +263,8 @@ function App() {
                 <s.TextTitle
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
                 >
-                  1 {CONFIG.SYMBOL} costs {CONFIG.DISPLAY_COST}{" "}
-                  {CONFIG.NETWORK.SYMBOL}.
+                  {CONFIG.DISPLAY_COST}{" "}
+                  {CONFIG.NETWORK.SYMBOL} per mint.
                 </s.TextTitle>
                 <s.SpacerXSmall />
                 <s.TextDescription
